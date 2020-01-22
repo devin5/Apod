144c5f310f580ff16e358bac23e5f7ee47dd319a
Devin Dias <devindias.tech@gmail.com>
2020-01-22 08:01:05 -0600

    initialized react app with cra

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p>
+        Read through the instructions in the README.md file to build your NASA
+        app! Have fun 🚀!
+      </p>
+    </div>
+  );
+}
+
+export default App;
